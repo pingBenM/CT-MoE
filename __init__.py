957b877cb76470e2ad9ca8c3a56e2042eeb7fb22
pingBenM <135229269+pingBenM@users.noreply.github.com>
2026-04-12 01:43:58 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .config import Config, VARIANTS, DISPLAY_NAMES, COLORS
-from .model import LanguageModel, MoELayer
-from .data import load_domain_texts, train_spm, TokenDataset
-from .train import train, eval_ppl, collect_diagnostics
